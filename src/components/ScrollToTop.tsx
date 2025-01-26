d0ef479c82e23a1fcaffcb14f86ab731242c7e68
Luis Jose Medina <luis06jose@gmail.com>
2025-01-25 20:18:27 -0400

    Corrección y mejora del componente `ScrollToTop`
    Mejora en el funcionamiento del componente `ScrollToTop`, de forma que
    se reinicie el contador para ocultar el botón luego de hacer "scroll" en
    la aplicación, esperando correctamente el tiempo establecido, contando
    desde el momento en el que se deja de hacer el "scroll".

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,22 +1,41 @@
 'use client'
 import { UpArrowIcon } from '@/assets/Icons'
 import { Button } from '@/components/Button'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export function ScrollToTop () {
+  const timeout = useRef<NodeJS.Timeout | null>(null)
   const [scroll, setScroll] = useState(0)
   const [show, setShow] = useState(false)
   const isBrowser = () => typeof window !== 'undefined' // The approach recommended by Next.js
 
-  function scrollToTop () {
+  const scrollToTop = () => {
     if (!isBrowser()) return
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
   useEffect(() => {
+    // Hide the button after X milliseconds
+    const setHideTimer = (milliseconds: number) => {
+      if (!isBrowser()) return
+
+      if (timeout.current == null) {
+        timeout.current = setTimeout(() => {
+          setShow(false)
+        }, milliseconds)
+      } else {
+        // Reset timer if it's already running
+        clearTimeout(timeout.current)
+        timeout.current = setTimeout(() => {
+          setShow(false)
+        }, milliseconds)
+      }
+    }
+
     const onScroll = () => {
       if (window.scrollY > 50) {
         setShow(true)
+        setHideTimer(3000)
       } else {
         setShow(false)
       }
@@ -26,15 +45,6 @@ export function ScrollToTop () {
 
     return () => { window.removeEventListener('scroll', onScroll) }
   }, [scroll])
-
-  // Hide the button after 3 seconds
-  useEffect(() => {
-    if (show) {
-      setTimeout(() => {
-        setShow(false)
-      }, 3000)
-    }
-  }, [show])
 
   return (
     <Button.Solid dark={true} size="small" onClick={scrollToTop} className={`${!show && 'translate-y-[200%]'} text-seasalt fixed shadow-lg bottom-6 right-6 rounded-full aspect-square z-50 transition-transform duration-300 md:bottom-7 lg:right-7`}>
